197e2088b94d48da49a785163054c95f5f9c12c9
donk23 <datadonk23@gmail.com>
2018-05-08 16:50:19 +0200

    Style setup

--- content/pages/index.md
+++ content/pages/index.md
@@ -1,10 +1,6 @@
-Title: Test index
+Title: Home
 URL:
 save_as: index.html
 Authors: Thomas Treml
 
-# TEST HEADER 1
-This is the content of my super blog post.
-
-## TEST HEADER 2
-asdfs safsd  dasf asafas as sadfasdf .
+Site under Construction
--- pelicanconf.py
+++ pelicanconf.py
@@ -19,17 +19,39 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
-
-DEFAULT_PAGINATION = 10
+DEFAULT_PAGINATION = 5
 
 # Uncomment following line if you want document-relative URLs when developing
-#RELATIVE_URLS = True
+RELATIVE_URLS = True
+
+LOAD_CONTENT_CACHE = False
+ARTICLE_URL = 'posts/{slug}.html'
+ARTICLE_SAVE_AS = 'posts/{slug}.html'
+TAGS_SAVE_AS = ''
+TAG_SAVE_AS = ''
+INDEX_SAVE_AS = 'blog_index.html'
+
+
+THEME = "../pelican-themes/my-pelican-bootstrap3"
+BOOTSTRAP_THEME = "flatly"
+JINJA_ENVIRONMENT = {'extensions': ['jinja2.ext.i18n']}
+PLUGIN_PATHS = ['../pelican-plugins']
+PLUGINS = ['i18n_subsites', 'pelican-ipynb.markup']
+MARKUP = ('md', 'ipynb')
+#IPYNB_IGNORE_CSS=True
+PYGMENTS_STYLE = 'github'
+STATIC_PATHS = ['images' , 'notebooks']
+
+#FAVICON = 'images/favicon.png'
+DISPLAY_CATEGORIES_ON_MENU = False
+CC_LICENSE = "CC-BY"
+SIDEBAR_ON_LEFT = True
+SOCIAL = (('twitter', 'https://twitter.com/datadonk23'),
+          ('linkedin', 'https://www.linkedin.com/in/thomastreml/', 'linkedin'),
+          ('github', 'https://github.com/donK23'),
+          ('facebook', 'https://www.facebook.com/treml.thomas', 'facebook'),
+          ('instagram', 'https://www.instagram.com/datadonk23/', 'instagram'),
+          ('medium', 'https://medium.com/@datadonk23', 'medium'))
+DISABLE_SIDEBAR_TITLE_ICONS = True
+ABOUT_ME = "<b>Data Scientist</b><br>Donkey hiker, cycler<br><small>Some shithole, AT</small>"
+AVATAR = "images/profile.png"
